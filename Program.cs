41089464217ae2d22e69db8d1c2f46d1217e353c
Lukas <strandlaefer@googlemail.com>
2019-11-05 14:30:56 +0100

    added .net core project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RingBuffer
+{
+    class PlayerSnapshot
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RingBuffer<PlayerSnapshot> ringBuffer = new RingBuffer<PlayerSnapshot>(20);
+            Console.WriteLine(ringBuffer.Capacity);
+        }
+    }
+}
---
+++ RingBuffer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RingBuffer
+{
+    public class RingBuffer<T>
+    {
+        public RingBuffer(int _capacity)
+        {
+            if (_capacity > 0)
+            {
+                Capacity = _capacity;
+                buffer = new T[_capacity];
+            }
+            else throw new ArgumentException("Must be greater than zero!", "_capacity");
+        }
+
+        #region Properties
+        public int Capacity { get; private set; }
+        #endregion
+
+        private T[] buffer;
+    }
+}
